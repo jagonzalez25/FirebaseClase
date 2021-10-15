2e6da5387fe2e35eb7b9eb6a68ee33da8765f4c0
jagonzalez25 <johans-123@hotmail.com>
2021-10-15 18:19:12 -0500

    Se agrega pagina  login

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyDWqoNnYqjy6tLmadPJU_sO3dDOHUP7A",
+  authDomain: "fir-clase-bc138.firebaseapp.com",
+  projectId: "fir-clase-bc138",
+  storageBucket: "fir-clase-bc138.appspot.com",
+  messagingSenderId: "993493277624",
+  appId: "1:993493277624:web:ccce35b624cf0c30120d4d",
+  measurementId: "G-4NNN050R18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
