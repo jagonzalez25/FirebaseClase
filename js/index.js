3193e4b4b8be6ff1cdff73217e76b45474c3974c
jagonzalez25 <johans-123@hotmail.com>
2021-10-29 18:51:59 -0500

    Se realia el registro en base de datos y se edita el nick y foto en autenticacion

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import {
     signInWithEmailAndPassword, TwitterAuthProvider, GithubAuthProvider, FacebookAuthProvider
 } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
 
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 import { getStorage, uploadBytesResumable, getDownloadURL, ref } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-storage.js";
 
 import { verAutenticacion } from "./firebase.js";
@@ -11,6 +11,7 @@ import { verAutenticacion } from "./firebase.js";
 const db = getFirestore();
 const storage = getStorage();
 var usarActual;
+var fotoActualizada = null;
 
 window.onload = function () {
     verAutenticacion();
@@ -185,6 +186,7 @@ function actualizarPerfil(user, providerName) {
 
 window.cambiarFoto = function cambiarFoto(archivo) {
 
+    document.getElementById("buttonEditPerfil").disabled = true;
     const file = archivo.files[0];
     const reader = new FileReader();
     reader.onloadend = function () {
@@ -200,16 +202,18 @@ window.cambiarFoto = function cambiarFoto(archivo) {
                 $('.progress-bar').css('width', progress + '%').attr('aria-valuenow', progress);
             },
             (error) => {
+                document.getElementById("buttonEditPerfil").disabled = false;
                 document.getElementById("progressUploadPhoto").style.visibility = "hidden";
                 $('.progress-bar').css('width', '0%').attr('aria-valuenow', 0);
                 document.getElementById("alertaActulizacionRegistro").style.display = "block";
                 document.getElementById("alertaActulizacionRegistro").innerHTML = error.message;
             },
             () => {
+                document.getElementById("buttonEditPerfil").disabled = false;
                 document.getElementById("progressUploadPhoto").style.visibility = "hidden";
                 $('.progress-bar').css('width', '0%').attr('aria-valuenow', 0);
                 getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    console.log('File available at', downloadURL);
+                    fotoActualizada = downloadURL;
                 });
             }
         );
@@ -220,6 +224,79 @@ window.cambiarFoto = function cambiarFoto(archivo) {
 
 window.editarPerfil = function editarPerfil() {
 
+    const displayName = document.getElementById("txtDisplayNameUpd").value;
+    const nombre = document.getElementById("txtnombre").value;
+    const apellido = document.getElementById("txtapellido").value;
+    const email = document.getElementById("txtemail").value;
+    const telefono = document.getElementById("txttelefono").value;
+    const provedor = document.getElementById("txtprovider").value;
+    let imgFoto = document.getElementById("imgFoto").src;
+  
+  
+    if (displayName == "") {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe ingresar un display Name";
+      return;
+    }
+    if (email == "") {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe ingresar un email";
+      return;
+    }
+    if (nombre == "") {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe ingresar un nombre";
+      return;
+    }
+    if (apellido == "") {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe ingresar un apellido";
+      return;
+    }
+    if (imgFoto.includes('asset/img/nouser.jpg')) {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe seleccionar una foto";
+      return;
+    }
+    if (telefono == "") {
+      document.getElementById("alertaActulizacionRegistro").style.display = "block";
+      document.getElementById("alertaActulizacionRegistro").innerHTML = "Debe ingresar un telefono";
+      return;
+    }
+  
+    if(fotoActualizada != null)
+         imgFoto = fotoActualizada;
 
 
+    setDoc(doc(db, "usuario", usarActual.uid), {
+            nombre: nombre,
+            apellido: apellido,
+            email: email,
+            displayName: displayName,
+            telefono: telefono,
+            provedor: provedor,
+            imgFoto: imgFoto,
+    }).then(() => {
+        editarAutorizacion(displayName, imgFoto);
+    }).catch((error) =>{
+        document.getElementById("alertaActulizacionRegistro").style.display = "block";
+        document.getElementById("alertaActulizacionRegistro").innerHTML = error.message;
+    });
+
+}
+
+function editarAutorizacion(displayName, photoURL) {
+
+    const auth =  getAuth();
+    updateProfile(auth.currentUser, {
+        displayName: displayName,
+        photoURL: photoURL
+    }).then(() => {
+        alert("Usuario editado correctamente");
+        document.location.href = "/";
+    }).catch((error) => {
+        const errorMessage = error.message;
+        document.getElementById("alertaActulizacionRegistro").style.display = "block";
+        document.getElementById("alertaActulizacionRegistro").innerHTML = errorMessage;
+    });
 }
